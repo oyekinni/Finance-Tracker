7a93573bcd098ed5182f2ea5604bcb45ce26d805
oyekinni <165745004+oyekinni@users.noreply.github.com>
2026-04-22 18:15:25 -0400

    FinanceTracker.java

--- FinanceTracker.java
+++ FinanceTracker.java
@@ -1,6 +1,7 @@
+// Main class that handles overall finance tracking
 public class FinanceTracker {
     private User user;
-// Main class that handles overall finance tracking
+
     public FinanceTracker(User user) {
         this.user = user;
     }
