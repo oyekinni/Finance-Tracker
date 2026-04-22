7dd4450eba794b94a142ef2041741102568a94b6
oyekinni <165745004+oyekinni@users.noreply.github.com>
2026-04-22 17:58:51 -0400

    User.java

---
+++ User.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private ArrayList<Transaction> transactions;
+
+    public User(String name) {
+        this.name = name;
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+    }
+
+    public double getBalance() {
+        double total = 0;
+        for (Transaction t : transactions) {
+            if (t.getType().equals("Income")) {
+                total += t.getAmount();
+            } else {
+                total -= t.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public void printTransactions() {
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
+}
