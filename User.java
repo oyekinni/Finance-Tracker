ce2d011f94af52cf782430af182c388d3853c174
oyekinni <165745004+oyekinni@users.noreply.github.com>
2026-04-27 11:50:57 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,31 +1,42 @@
 import java.util.ArrayList;
 
+// Stores user information and manages transactions
 public class User {
+
     private String name;
     private ArrayList<Transaction> transactions;
- // Stores user information and manages transactions
 
     public User(String name) {
         this.name = name;
         transactions = new ArrayList<>();
     }
 
+    // Returns the list of transactions
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    // Adds a new transaction
     public void addTransaction(Transaction t) {
         transactions.add(t);
     }
 
+    // Calculates total balance
     public double getBalance() {
         double total = 0;
+
         for (Transaction t : transactions) {
-            if (t.getType().equals("Income")) {
+            if (t.getType().equalsIgnoreCase("Income")) {
                 total += t.getAmount();
             } else {
                 total -= t.getAmount();
             }
         }
+
         return total;
     }
 
+    // Prints all transactions
     public void printTransactions() {
         for (Transaction t : transactions) {
             System.out.println(t);
